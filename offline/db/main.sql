2d2f3126308c3ed12a1e4667f6e563b06cc5b9b7
zhanghao <zhanghao_py@sina.com>
2013-12-10 15:30:30 +0800

    update sql

--- offline/db/main.sql
+++ offline/db/main.sql
@@ -1,37 +1,42 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : 172.18.4.82_8306
+ Source Server Version : 50169
+ Source Host           : 172.18.4.82
+ Source Database       : bjut_help
+
+ Target Server Version : 50169
+ File Encoding         : utf-8
+
+ Date: 12/10/2013 15:28:05 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for `tb_role`
+--  Table structure for `tb_comment`
 -- ----------------------------
-DROP TABLE IF EXISTS `tb_role`;
-CREATE TABLE `tb_role` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) CHARACTER SET utf8 NOT NULL,
+DROP TABLE IF EXISTS `tb_comment`;
+CREATE TABLE `tb_comment` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `user_id` int(10) NOT NULL COMMENT 'user表外键',
+  `message_id` int(10) NOT NULL COMMENT 'message表外键',
+  `content` varchar(255) NOT NULL COMMENT '评论内容',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '时间戳',
+  `is_read` tinyint(1) NOT NULL COMMENT '0-已读 1-未读',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of tb_role
+--  Table structure for `tb_message`
 -- ----------------------------
-INSERT INTO `tb_role` VALUES ('1', 'ROLE_ADMIN');
-INSERT INTO `tb_role` VALUES ('2', 'ROLE_USER');
-
--- ----------------------------
--- Table structure for `tb_user`
--- ----------------------------
-DROP TABLE IF EXISTS `tb_user`;
-CREATE TABLE `tb_user` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `enabled` tinyint(3) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 DROP TABLE IF EXISTS `tb_message`;
 CREATE TABLE `tb_message` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `user_id` int(10) NOT NULL COMMENT 'user表外键',
-  `timestamp` TIMESTAMP(50) NOT NULL COMMENT '时间戳',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '时间戳',
   `content` varchar(255) NOT NULL COMMENT '消息内容',
   `type` varchar(255) NOT NULL COMMENT '0-文字消息 1-语音消息',
   `expire` int(10) NOT NULL COMMENT '过期时间（单位：秒）',
@@ -40,38 +45,76 @@ CREATE TABLE `tb_message` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `tb_comment`;
-CREATE TABLE `tb_comment` (
-  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `user_id` int(10) NOT NULL COMMENT 'user表外键',
-  `message_id` int(10) NOT NULL COMMENT 'message表外键',
-  `content` varchar(255) NOT NULL COMMENT '评论内容',
-  `timestamp` TIMESTAMP(50) NOT NULL COMMENT '时间戳',
-  `is_read` tinyint(1) NOT NULL COMMENT '0-已读 1-未读',
+-- ----------------------------
+--  Table structure for `tb_resource`
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_resource`;
+CREATE TABLE `tb_resource` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `url` varchar(255) NOT NULL COMMENT 'url',
+  `auth` varchar(255) NOT NULL COMMENT '权限',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='资源表';
 
 -- ----------------------------
--- Records of user
+--  Records of `tb_resource`
 -- ----------------------------
-INSERT INTO `tb_user` VALUES ('1', 'zhanghao', 'e10adc3949ba59abbe56e057f20f883e', '1');
-INSERT INTO `tb_user` VALUES ('3', 'hujiyun', '3f5917e2b6a6fc116b8d63f0f16c7323', '1');
-INSERT INTO `tb_user` VALUES ('4', 'majikun', '487e33aec5da39ea69f08c62f19223e1', '1');
+BEGIN;
+INSERT INTO `tb_resource` VALUES ('1', '/demo/**', 'ROLE_USER');
+COMMIT;
 
 -- ----------------------------
--- Table structure for `tb_user_role`
+--  Table structure for `tb_role`
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_role`;
+CREATE TABLE `tb_role` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `tb_role`
+-- ----------------------------
+BEGIN;
+INSERT INTO `tb_role` VALUES ('1', 'ROLE_ADMIN'), ('2', 'ROLE_USER');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `tb_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `enabled` tinyint(3) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `tb_user`
+-- ----------------------------
+BEGIN;
+INSERT INTO `tb_user` VALUES ('1', 'zhanghao', '1e19be6365d97385f9c623ec0efb354c', '1'), ('3', 'hujiyun', '3f5917e2b6a6fc116b8d63f0f16c7323', '1'), ('4', 'majikun', '487e33aec5da39ea69f08c62f19223e1', '1'), ('5', 'renbiyi', '4628ad60cf1099d001c65f8c6528d32d', '1'), ('6', 'cuijian', '8df77c1d646f1f942cde57a2a285fd84', '1');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `tb_user_role`
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_user_role`;
 CREATE TABLE `tb_user_role` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `uid` int(10) NOT NULL,
-  `rid` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `role_id` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of user_role
+--  Records of `tb_user_role`
 -- ----------------------------
-INSERT INTO `tb_user_role` VALUES ('1', '1', '1');
-INSERT INTO `tb_user_role` VALUES ('2', '4', '1');
-INSERT INTO `tb_user_role` VALUES ('3', '1', '2');
+BEGIN;
+INSERT INTO `tb_user_role` VALUES ('1', '1', '1'), ('2', '4', '1'), ('3', '1', '2'), ('4', '5', '2'), ('5', '6', '2');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
