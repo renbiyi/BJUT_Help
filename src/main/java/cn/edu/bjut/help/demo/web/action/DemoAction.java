7ff1ea19b62e84bd6dc0cf627bdbb1ae9fc3acbb
zhanghao <zhanghao_py@sina.com>
2013-12-12 19:42:32 +0800

    获得登录后用户名

--- src/main/java/cn/edu/bjut/help/demo/web/action/DemoAction.java
+++ src/main/java/cn/edu/bjut/help/demo/web/action/DemoAction.java
@@ -3,6 +3,8 @@ package cn.edu.bjut.help.demo.web.action;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +19,13 @@ public class DemoAction extends BaseAction {
 	@RequestMapping(value = "hellojson", method = RequestMethod.GET) 
 	@ResponseBody
     public Map<String, Object> hellojson() {  
+		
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		logger.info("current user is " + username);
+		
+//		SecurityContext sctx = (SecurityContext) session.getAttribute("SPRING_SECURITY_CONTEXT");
+//		logger.info("current user[session get] is " + sctx.getAuthentication().getName());
+		
 		Map<String, Object> jsonMap = new HashMap<String, Object>();
 		jsonMap.put("status", 200);
 		jsonMap.put("data", "zhanghao");
