b837f5d882cf98bd03137a07fc2b617e7151c185
zhanghao <zhanghao_py@sina.com>
2013-12-15 15:42:07 +0800

    加入上传文件相关jar

--- src/main/java/cn/edu/bjut/help/demo/web/action/DemoAction.java
+++ src/main/java/cn/edu/bjut/help/demo/web/action/DemoAction.java
@@ -1,35 +1,57 @@
 package cn.edu.bjut.help.demo.web.action;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.security.core.context.SecurityContext;
+import org.apache.commons.io.FileUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import cn.edu.bjut.help.core.web.action.BaseAction;
 
-@Controller  
+@Controller
 @RequestMapping("demo")
 public class DemoAction extends BaseAction {
 
-	@RequestMapping(value = "hellojson", method = RequestMethod.GET) 
+	@RequestMapping(value = "hellojson", method = RequestMethod.GET)
 	@ResponseBody
-    public Map<String, Object> hellojson() {  
-		
-		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+	public Map<String, Object> hellojson() {
+
+		String username = SecurityContextHolder.getContext()
+				.getAuthentication().getName();
 		logger.info("current user is " + username);
-		
-//		SecurityContext sctx = (SecurityContext) session.getAttribute("SPRING_SECURITY_CONTEXT");
-//		logger.info("current user[session get] is " + sctx.getAuthentication().getName());
-		
+
+		// SecurityContext sctx = (SecurityContext)
+		// session.getAttribute("SPRING_SECURITY_CONTEXT");
+		// logger.info("current user[session get] is " +
+		// sctx.getAuthentication().getName());
+
 		Map<String, Object> jsonMap = new HashMap<String, Object>();
 		jsonMap.put("status", 200);
 		jsonMap.put("data", "zhanghao");
+
+		return jsonMap;
+	}
+
+	@RequestMapping(value = "upload", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> handleAnotherUploadProcess() throws Exception {
+		MultipartHttpServletRequest req = (MultipartHttpServletRequest) request;
+		MultipartFile file = req.getFile("imageFile");
+		File output = new File("/Users/zhanghao/" + file.getOriginalFilename());
+		FileUtils.copyInputStreamToFile(file.getInputStream(), output);
 		
-        return jsonMap;
-    }
+		Map<String, Object> jsonMap = new HashMap<String, Object>();
+		jsonMap.put("status", 200);
+		jsonMap.put("data", "upload sucessful");
+		
+		return jsonMap;
+	}
+	
 }
