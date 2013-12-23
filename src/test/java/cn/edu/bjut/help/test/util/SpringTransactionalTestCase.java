ccf58f7e384ecd45058296de27491d4501784b2d
zhanghao <zhanghao_py@sina.com>
2013-12-23 19:46:08 +0800

    更新单元测试

--- src/test/java/cn/edu/bjut/help/test/util/SpringTransactionalTestCase.java
+++ src/test/java/cn/edu/bjut/help/test/util/SpringTransactionalTestCase.java
@@ -1,5 +1,6 @@
 package cn.edu.bjut.help.test.util;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -12,4 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional 
 public class SpringTransactionalTestCase {
 
+    @Test
+    public void doNothing() {
+    	
+    }
 }
